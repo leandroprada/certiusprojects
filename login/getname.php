5764f0a552007ca1af7ccd58d5bc18ab96222d7f
leandroprada <leandroprada@gmail.com>
2020-03-12 15:43:46 -0300

    probando logo en login

--- login/getname.php
+++ login/getname.php
@@ -5,46 +5,14 @@
 require 'dbconn.inc.php';
 $email = $_SESSION['email'];
 
- // $sql = "SELECT email FROM usr WHERE email=?";
- $sql = "SELECT * FROM usr WHERE email=?";
- $stmt = mysqli_stmt_init($conn);
- if (!mysqli_stmt_prepare($stmt, $sql)){
-	  header("Location: login.php?error=wrongusername");
-	  echo "Cannot access database";
-	  exit();
-	  }
-	  	  else {
-		  $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-		  
-		  mysqli_stmt_bind_param ($stmt, "s", $email);
-		  mysqli_stmt_execute($stmt);
-		  mysqli_stmt_store_result($stmt);
-		  $result = mysqli_stmt_store_result($stmt);
-		  $resultCheck = mysqli_stmt_num_rows($stmt);
-// test code for debugging
-		  mysqli_stmt_fetch($stmt);
-		  echo "luego de statement fetch";
-		  var_dump ($stmt);
-		  $row = $stmt[1];
-		  var_dump ($row);
-		  
-//  end of test code for debugging		  
-		  if ($resultCheck >= 1) {
-				$_SESSION['login'] = "validated";
-				$userloggedname = $_SESSION['name'];
-				echo $userloggedname;
-				
-			   // header("Location: getname.php");
-		  }
-		  else {
-			  header("Location: ../index.php?error=sqlcouldnotexecute");
-		  }
-
-	  }
-
-
+<?php
+$query = "'SELECT name FROM usr WHERE email=?'.$email";
+echo $query;
+$queryResult = $conn->query($query);
+echo $queryResult;
 ?>
 
+ 
 
 <!DOCTYPE html>
 <html lang="en">
