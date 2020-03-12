1eaadc7e02e1467e900d08f6222bb0861ba1246b
leandroprada <leandroprada@gmail.com>
2020-03-11 21:46:05 -0300

    troubleshooting login.php

--- anses_ivr.php
+++ anses_ivr.php
@@ -22,14 +22,14 @@ $_SESSION['email'] = $_POST['email'];
 
 		<?php
 												 if (($_SESSION['login']) != "validated") {
-												 echo '<div class="wrapper d-flex align-items-stretch">Invalid Email - Please try again -Click <a href="./login/index.php" >&nbsp;Here</a></div>';}
+												 echo <div class="wrapper d-flex align-items-stretch">Invalid Email - Please try again -Click <a href="./login/index.php" >&nbsp;Here</a></div>;}
 												
 												
 												?>
 	
 			<?php
 			else echo 			
-		'<div class="wrapper d-flex align-items-stretch">			
+		<div class="wrapper d-flex align-items-stretch">			
 			<nav id="sidebar">
 				<div class="p-4 pt-5">
 		  		<a href="#" class="img logo rounded-circle mb-5" style="background-image: url(images/logo.jpg);"></a>
@@ -218,7 +218,7 @@ $_SESSION['email'] = $_POST['email'];
         
       </div>
 		</div>
-';?>
+
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.js"></script>
     <script src="js/bootstrap.min.js"></script>
