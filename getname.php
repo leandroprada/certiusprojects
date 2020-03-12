a5e133f6caa085c868b58768ce3d2dc905acd890
leandroprada <leandroprada@gmail.com>
2020-03-12 12:41:34 -0300

    probando logo en login

--- getname.php
+++ getname.php
@@ -19,16 +19,17 @@ $email = $_SESSION['email'];
 		  mysqli_stmt_execute($stmt);
 		  mysqli_stmt_store_result($stmt);
 		  $resultCheck = mysqli_stmt_num_rows($stmt);
+		  
 		  if ($resultCheck >= 1) {
-			  
+				$_SESSION['login'] = "validated";
+				$_SESSION['email'] = $email;
 				$result = mysqli_stmt_get_result($stmt);
-				while ($row = $result->fetch_assoc()) 
-        {
-
-            echo 'name: '.$row['name'].'<br>';
-
-        }
-
+				$result_row =  mysqli_stmt_fetch($stmt);
+			   	$result = mysqli_stmt_get_result($stmt);
+				
+				echo ("%s (%s)\n", $name, $code);
+				
+				mysqli_stmt_close($stmt);
         
 				$_SESSION['login'] = "validated";
 				
