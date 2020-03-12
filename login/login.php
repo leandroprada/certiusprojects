ab11a914a286e5839fdedd81d6c1edbf877f636b
leandroprada <leandroprada@gmail.com>
2020-03-12 08:40:05 -0300

    probando logo en login

--- login/login.php
+++ login/login.php
@@ -32,65 +32,94 @@ $_SESSION['email'] = $_POST['email'];
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="../css/styleprojects.css">
 <!--===============================================================================================-->
 </head>
 <body>
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
-				<form class="login100-form validate-form flex-sb flex-w" action="signup.php" method="post">
-					<span class="login100-form-title p-b-32">
-						Login - <?php
-													 if (($_SESSION['login']) != "validated") {
-													 echo '<span style="font-size:0.3em;"> Invalid Email - Please try again</span>';}
+	<div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar">
+				<div class="p-4 pt-5">
+		  		<a href="#" class="img logo rounded-circle mb-5" style="background-image: url(../images/logo.jpg);"></a>
+	        <ul class="list-unstyled components mb-5">
+	          <li>
+	            <a href="../index.php" aria-expanded="false" >Go back to Certius Projects</a>
+	          </li>
+			            
+	        </ul>
 
-													?>
-					</span>
+	        <div class="footer" >
+	        	<p style="color:#024f05;"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+						  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+	        </div>
 
-					<span class="txt1 p-b-11">
-						Username
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "Username is required">
-						<input class="input100" type="text" name="username" >
-						<span class="focus-input100"></span>
-					</div>
+	      </div>
+    	</nav>
 
-					<span class="txt1 p-b-11">
-						Password
-					</span>
-					<div class="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
-						<span class="btn-show-pass">
-							<i class="fa fa-eye"></i>
-						</span>
-						<input class="input100" type="password" name="password" >
-						<span class="focus-input100"></span>
-					</div>
+        <!-- Page Content  -->
+      <div id="content" class="p-4 p-md-5">
 
-					<div class="flex-sb-m w-full p-b-48">
-						<div class="contact100-form-checkbox">
-							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
-								Remember me
-							</label>
+        <div class="limiter">
+						<div class="container-login100">
+							<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+								<form class="login100-form validate-form flex-sb flex-w" action="signup.php" method="post">
+									<span class="login100-form-title p-b-32">
+										Login - <?php
+																	 if (($_SESSION['login']) != "validated") {
+																	 echo '<span style="font-size:0.3em;"> Invalid User - Please try again</span>';}
+
+																	?>
+									</span>
+
+									<span class="txt1 p-b-11">
+										Email
+									</span>
+									<div class="wrap-input100 validate-input m-b-36" data-validate = "Email is required">
+										<input class="input100" type="text" name="email" >
+										<span class="focus-input100"></span>
+									</div>
+
+									<span class="txt1 p-b-11">
+										Password
+									</span>
+									<div class="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
+										<span class="btn-show-pass">
+											<i class="fa fa-eye"></i>
+										</span>
+										<input class="input100" type="password" name="password" >
+										<span class="focus-input100"></span>
+									</div>
+
+									<div class="flex-sb-m w-full p-b-48">
+										<div class="contact100-form-checkbox">
+											<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+											<label class="label-checkbox100" for="ckb1">
+												Remember me
+											</label>
+										</div>
+
+										<div>
+											<a href="#" class="txt3">
+												Forgot Password?
+											</a>
+										</div>
+									</div>
+
+									<div class="container-login100-form-btn">
+										<button class="login100-form-btn" type="submit" name ="signup-submit">
+											Login
+										</button>
+									</div>
+
+								</form>
+							</div>
 						</div>
-
-						<div>
-							<a href="#" class="txt3">
-								Forgot Password?
-							</a>
-						</div>
 					</div>
-
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" type="submit" name ="signup-submit">
-							Login
-						</button>
-					</div>
-
-				</form>
+        
 			</div>
 		</div>
-	</div>
+
+	
 
 
 	<div id="dropDownSelect1"></div>
