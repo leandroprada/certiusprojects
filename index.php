f083ad1186d49a6e22253aa4685c37e88cd17623
leandroprada <leandroprada@gmail.com>
2020-03-12 08:58:45 -0300

    probando logo en login

--- index.php
+++ index.php
@@ -15,73 +15,22 @@
 		<link rel="stylesheet" href="./css/styleprojects.css">
   </head>
   <body>
-		<? if (($_SESSION['login']) != 'validated')): echo?>
-  
-	<div class="wrapper d-flex align-items-stretch">
-			<nav id="sidebar">
-				<div class="p-4 pt-5">
-		  		<a href="#" class="img logo rounded-circle mb-5" style="background-image: url(images/logo.jpg);"></a>
-				
-				
-				<? if (($_SESSION['login']) != 'validated')): {echo?>
-				  <p>Not Logged In</p>
-				<?} elseif (($_SESSION['login']) = 'validated')): {echo?>
-				  <p>Logged in</p>
-				<?} else: {echo?>
-				  <p>Default Content</p>
-				<?} endif; ?>
-								
-	        
-
-	        <div class="footer" >
-	        	<p style="color:#024f05;"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-						  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	        </div>
-
-	      </div>
-    	</nav>
-
-        <!-- Page Content  -->
-      <div id="content" class="p-4 p-md-5">
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-
-            <button type="button" id="sidebarCollapse" class="btn btn-primary">
-              <i class="fa fa-bars"></i>
-              <span class="sr-only">Toggle Menu</span>
-            </button>
-            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa fa-bars"></i>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="./login/index.php">Login</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <h2 class="mb-4">Certius Projects</h2>
-        <p>In this page you will find all non-Avaya project documentation</p>
-        <p>Please <a  class="btn-primary"href="./login/index.php">Login</a></p>
 		
-        
-      </div>
-	</div>
-  
-  
-  
-<?; elseif (($_SESSION['login']) = 'validated')): echo?>
+
 		
 		<div class="wrapper d-flex align-items-stretch">
 			<nav id="sidebar">
 				<div class="p-4 pt-5">
 		  		<a href="#" class="img logo rounded-circle mb-5" style="background-image: url(images/logo.jpg);"></a>
+				
+				<? if (($_SESSION['login']) != 'validated')): {echo$_SESSION['login']?>
+				  <p>Not Logged In</p>
+				<?} elseif (($_SESSION['login']) = 'validated')): {echo$_SESSION['login']?>
+				  <p>Logged in</p>
+				<?} else: {echo$_SESSION['login']?>
+				  <p>Default Content</p>
+				<?} endif; ?>
+				
 	        <ul class="list-unstyled components mb-5">
 	          <li>
 	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Internal Projects</a>
@@ -184,9 +133,7 @@
       </div>
 		</div>
 		
-	<?; else: echo?>
-  <p>Default Content</p>
-	<? endif; ?>
+
 
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.js"></script>
