ecb191a1f4cafb1882dd92c82f9104adf7a52fe1
leandroprada <leandroprada@gmail.com>
2020-03-13 15:20:00 -0300

    dando forma a los includes de partes comunes

--- content/content_ansesIvr.php
+++ content/content_ansesIvr.php
@@ -11,11 +11,28 @@
 	
     <h3 class="mb-4">Documentation</h3>
 		<ul>
-		<li>Project Notes: - <a href="../files/teleperformance/">Project Notes</a></li>
-		<li>Customer acceptance: - <a href="../files/teleperformance/project_acceptance.msg">Acceptance Email</a></li>
-		
+		<li> <h3 class="mb-4">General</h3>
+			<ul>
+			<li>Project Notes: - <a href="../files/teleperformance/Project Notes_9.0.docx">Project Notes</a></li>
+			<li>Customer acceptance: - <a href="../files/teleperformance/project_acceptance.msg">Acceptance Email</a></li>
+			
+			</ul>
+		</li>
 		</ul>
-
+		
+		<ul>
+		<li> <h3 class="mb-4">Qa</h3>
+			<ul>
+			<li>LIST OF QA TEST CASES: - Survey<a href="../files/teleperformance/18TP0001-TEST-QA-0001_1.0.xlsx">18TP0001-TEST-QA-0001_1.0.xlsx</a></li>
+			<li>LIST OF QA TEST CASES: - Survey2<a href="../files/teleperformance/project_acceptance.msg">18TP0002-TEST-QA-0001_1.0.xlsx</a></li>
+			<li>LIST OF QA TEST CASES: - Log<a href="../files/teleperformance/survey_qa_10052018.txt">survey_qa_10052018.txt</a></li>
+			
+			</ul>
+		</li>
+		</ul>
+		
+		
+		
 	  ';
 	  ?>
 		
