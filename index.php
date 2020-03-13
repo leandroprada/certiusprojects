dd97b6ddad230a03155f563072ac43f379439e26
leandroprada <leandroprada@gmail.com>
2020-03-13 12:12:48 -0300

    dando forma a los includes de partes comunes

--- index.php
+++ index.php
@@ -107,9 +107,9 @@
               <i class="fa fa-bars"></i>
               <span class="sr-only">Toggle Menu</span>
             </button>
-            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <!--<button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa fa-bars"></i>
-            </button>
+            </button>-->
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="nav navbar-nav ml-auto">
@@ -165,14 +165,14 @@
         <!-- Page Content  -->
       <div id="content" class="p-4 p-md-5">
 
-        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
 
             <button type="button" id="sidebarCollapse" class="btn btn-primary">
               <i class="fa fa-bars"></i>
               <span class="sr-only">Toggle Menu</span>
             </button>
-            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <!--<button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa fa-bars"></i>
             </button>
 
@@ -183,9 +183,9 @@
                 </li>
                 
               </ul>
-            </div>
+            </div> -->
           </div>
-        </nav> -->
+        </nav> 
 		<h2>This is a restricted area - Only Authorized Accounts</h2>
         <p class="mb-4">Please login
         <a class="btn btn-dark" href="./login/login.php" > Here </a></p>
