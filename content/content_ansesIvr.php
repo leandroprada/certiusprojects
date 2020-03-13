ebcc364303cd00bac9822911d60ddc3ac82cf5de
leandroprada <leandroprada@gmail.com>
2020-03-13 15:09:22 -0300

    dando forma a los includes de partes comunes

--- content/content_ansesIvr.php
+++ content/content_ansesIvr.php
@@ -4,9 +4,17 @@
  $userlogged = $_SESSION['name'];
  
 	echo '
+	<img src="../files/teleperformance/tp_logo.jpg">
+	
 	 <h2 class="mb-4">18TP0001 - Teleperformance Anses IVR</h2>
         <p>In this page you will find all documentation Teleperformance Anses IVR</p>
+	
+    <h3 class="mb-4">Documentation</h3>
+		<ul>
+		<li>Project Notes: - <a href="="../files/teleperformance/"</li>
+		<li>Customer acceptance: - <a href="="../files/teleperformance/project_acceptance.msg"</li>
 		
+		</ul>
 
 	  ';
 	  ?>
