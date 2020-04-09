603c6aca155eb87ec7327087c73a3257351850d1
leandroprada <leandroprada@gmail.com>
2020-04-09 10:37:21 -0300

    updated wyndham VUIs translated and 20WT0001-PL-0001_3.0

--- content/content_wyndham.php
+++ content/content_wyndham.php
@@ -43,9 +43,10 @@
 			<li><h3 class="mb-4">Certius</h3>
 				<ul> 
 				<li><a href="../files/waterfield/tech_proposal_revC.docx">Certius Quotation Tech Only Rev C</a></li>
-				<li><a href="../files/waterfield/20WT0001-PL-0001_2.5.xlsx">20WT0001-PL-0001_2.5.xlsx</a></li>
+				<li><a href="../files/waterfield/20WT0001-PL-0001_3.0.xlsx">20WT0001-PL-0001_3.0.xlsx</a></li>
 				<li><a href="../files/waterfield/AlphaFile.zip">Alphas grammars (English Spanish Portuguese)</a></li>
 				<li><a href="../files/waterfield/Wyndham_VUI_20200403_ES.docx">Wyndham_VUI_20200403_ES.docx</a></li>
+				<li><a href="../files/waterfield/Wyndham_VUI_20200403_PT.docx">Wyndham_VUI_20200403_PT.docx</a></li>
 				
 				<li><h5>Superseded</h5>
 				<ul>
@@ -54,6 +55,7 @@
 				<li><a href="../files/waterfield/old/20WT0001-PL-0001_2.2.xlsx">20WT0001-PL-0001_2.2.xlsx</a></li>
 				<li><a href="../files/waterfield/old/20WT0001-PL-0001_2.3.xlsx">20WT0001-PL-0001_2.3.xlsx</a></li>
 				<li><a href="../files/waterfield/old/20WT0001-PL-0001_2.4.xlsx">20WT0001-PL-0001_2.4.xlsx</a></li>
+				<li><a href="../files/waterfield/old/20WT0001-PL-0001_2.5.xlsx">20WT0001-PL-0001_2.5.xlsx</a></li>
 				
 				<li><a href="../files/waterfield/CertiusTechnologies-TranslationsToPorgutuesePart2RevA.docx">CertiusTechnologies-TranslationsToPorgutuesePart2RevA.docx</a></li>
 				
