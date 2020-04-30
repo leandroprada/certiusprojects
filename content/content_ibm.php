1e704c4cfc26bfb0f07ec89d03eafe2e5685f76e
leandroprada <leandroprada@gmail.com>
2020-04-30 15:26:11 -0300

    added IBM

--- content/content_ibm.php
+++ content/content_ibm.php
@@ -17,17 +17,9 @@
 				<li><a href="https://cloud.ibm.com/docs/services/assistant?topic=assistant-getting-started#getting-started" target="_blank">Getting started with Watson Assistant</a></li>
 				
 				<li><h5>Other documentation</h5>
-				/* <ul>
-				<li><a href="../files/waterfield/old/WVO.vsd">Initial Call Flow Visio</a></li>
-				<li><a href="../files/waterfield/old/Wyndhamcallflow1.22.20.pdf">Wyndham call flow 1.22.20.pdf</a></li>
-				<li><a href="../files/waterfield/old/Wyndhamcallflow2.4.20.pdf">Wyndham call flow 2.4.20.pdf</a></li>
-				<li><a href="../files/waterfield/old/Wyndhamcallflow4.10.20.pdf">Call Flow Version 4.10.20</a></li>
-				<li><a href="../files/waterfield/old/Wyndham_VUI_20200102.docx">Wyndham_VUI_20200102.docx</a></li>
-				<li><a href="../files/waterfield/old/Wyndham_VUI_20200302.docx">Wyndham_VUI_20200302.docx</a></li>
-				<li><a href="../files/waterfield/old/Wyndham_VUI_20200403.docx">Wyndham_VUI_20200403.docx</a></li>
-				<li><a href="../files/waterfield/old/Wyndhamcallflow3.5.20.pdf">Call Flow Version 3.5.20</a></li>
-				<li><a href="../files/waterfield/old/Wyndhamphraselistv5.xlsx">Wyndhamphraselistv5.xlsx</a></li>
-				</ul> */
+				<ul>
+				<li><a href="">More Information</a></li>
+				</ul>
 				</li>
 				</ul>
 			</li>
--- content/content_sidebar_logged_in.php
+++ content/content_sidebar_logged_in.php
@@ -32,7 +32,7 @@
 	         </li>
 			 
 			 <li>
-              <a href="#homeSubmenu4" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Watson Projects</a>
+              <a href="#homeSubmenu4" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">IBM Projects</a>
               <ul class="collapse list-unstyled" id="homeSubmenu4">
                 <li>
                     <a href="./ibm.php">20IB0001 - Watson Prototype</a>
