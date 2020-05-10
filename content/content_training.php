7ed15250e62a5cc4e8c8a2a302f9bb28d0dbf4dc
leandroprada <leandroprada@gmail.com>
2020-05-10 20:44:04 -0300

    added training

--- content/content_training.php
+++ content/content_training.php
@@ -10,9 +10,9 @@
         <div class="row">
           <div class="col-md-12 col-sm-12">
               <div class="video-promo-content text-center">
-                <iframe width="75%"	 height="450px" src="https://youtu.be/5GhmKXS0aYo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
-                <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Presentaci&oacute;n</h2>
-                <p class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Son s&oacute;lo dos minutos</p>
+                <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Tomcat training</h2>
+                <object width="600px" height="450px" data="../files/training/tomcatTraining.mp4"></object>
+				
               </div>
           </div>
         </div>
