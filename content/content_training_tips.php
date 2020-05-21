12f530c066a7f92c2383856a5a4fd8f4cccf11ae
leandroprada <leandroprada@gmail.com>
2020-05-21 10:53:38 -0300

    adding the training session

--- content/content_training_tips.php
+++ content/content_training_tips.php
@@ -11,6 +11,8 @@
           <div class="col-md-12 col-sm-12">
               <div class="text-center">
                 <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Training Tips</h2>
+				Tip <a href="../content/tips/A01.ppt" class="btn btn-info" style="color:white;">A01</a><a  href="../content/tips/A01.ppt" target="_blank"> - fuera del ul y NO EN UN DIV</a>
+				Tip A01 - Arquitectura Voice Portal RevB <a href="../content/tips/A01.ppt" class="btn btn-info" style="color:white;">Ver</a>
 				<div style="text-decoration:none">
 						<a href="../content/tips/A02.ppt" class="btn btn-info" style="color:white;">Tip A02</a><a  href="../content/tips/A02.ppt" target="_blank"> - Fuera del UL</a>
 				</div>
