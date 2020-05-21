b8d34d55dd156615f44740864a147b26a8d652c0
leandroprada <leandroprada@gmail.com>
2020-05-21 09:50:33 -0300

    adding the training session

--- content/content_training_tips.php
+++ content/content_training_tips.php
@@ -11,7 +11,7 @@
           <div class="col-md-12 col-sm-12">
               <div class="text-center">
                 <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Training Tips</h2>
-                <ul>
+                <ul style="text-decoration:none">
 					<li>
 						<a class="btn btn-info" style="color:white;">Tip A01</a><a  href="../content/tips/A01.ppt" target="_blank"> - Arquitectura Voice Portal RevB</a>
 					</li>
