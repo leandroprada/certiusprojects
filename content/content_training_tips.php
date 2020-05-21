953a992e2dc40165b32bc60b404274d9efb621d6
leandroprada <leandroprada@gmail.com>
2020-05-21 11:00:49 -0300

    adding the training session

--- content/content_training_tips.php
+++ content/content_training_tips.php
@@ -11,15 +11,15 @@
           <div class="col-md-12 col-sm-12">
               <div class="text-center">
                 <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Training Tips</h2>
-				
-				<div style="text-decoration:none">
+				<ul style="list-style:none">
+				<li style="text-decoration:none">
 						A01 - Arquitectura Voice Portal RevB <a href="../content/tips/A01.ppt" class="btn btn-info" style="color:white;">Ver</a>
-				</div>
-				<div style="text-decoration:none">
+				</li>
+				<li style="text-decoration:none">
 						A02 - Instalacion Eclipse-Tomat RevD <a href="../content/tips/A02.ppt" class="btn btn-info" style="color:white;">Ver</a>
-				</div>
+				</li>
 				
-				
+				</ul>
 									
 					
               </div>
