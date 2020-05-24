ec9c6a68be682ec2442b19c0adda47a37b5b5617
leandroprada <leandroprada@gmail.com>
2020-05-23 23:51:39 -0300

    edited IBM permission

--- content/content_ibm.php
+++ content/content_ibm.php
@@ -1,10 +1,25 @@
 <?php
 
  session_start();
+require 'dbconn.inc.php';
  $userlogged = $_SESSION['name'];
- 
-	echo '
-	 <h2 class="mb-4">20IB0001 IBM - Watson Project</h2>
+$permissions = $_SESSION['permissions'];
+	//The code below parses course list and loops through it	
+	$permissions = explode(",",$permissions);
+
+foreach ($permissions as $p_id) 
+	{
+		$querycd1 = "SELECT permission_description FROM permissions WHERE p_id=";
+		$querycd2 = '"'.$p_id.'";';	
+		$querycd = $querycd1.$querycd2;
+		$resultcd = mysqli_query($conn,$querycd);
+		$rowcd = mysqli_fetch_row($resultcd);
+		$permissionname = $rowcd[0];
+
+	if ($permissionname != NULL) {
+		if ($permissionname == "projects"){
+		echo '	
+			 <h2 class="mb-4">20IB0001 IBM - Watson Project</h2>
         
         <p>In this page you will find all documentation for the IBM Watson pilot project</p>
 		
@@ -31,6 +46,16 @@
 		<h3 class="mb-4">Additional Information</h3>
 		<p>Soon more information...</p>
 		
+		';
+		}
+		
+		}
+	}
+	  ?>
+
+
+	echo '
+
 	  ';
 	  ?>
 		
