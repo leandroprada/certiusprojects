95f7c70294328ca07429aaeb0b9ff80fc7c40844
leandroprada <leandroprada@gmail.com>
2020-05-24 00:03:24 -0300

    edited  permission for all content_pages

--- content/content_training_videos.php
+++ content/content_training_videos.php
@@ -1,9 +1,23 @@
 <?php
-
  session_start();
+require 'dbconn.inc.php';
  $userlogged = $_SESSION['name'];
- 
-	echo '
+$permissions = $_SESSION['permissions'];
+	//The code below parses course list and loops through it	
+	$permissions = explode(",",$permissions);
+
+foreach ($permissions as $p_id) 
+	{
+		$querycd1 = "SELECT permission_description FROM permissions WHERE p_id=";
+		$querycd2 = '"'.$p_id.'";';	
+		$querycd = $querycd1.$querycd2;
+		$resultcd = mysqli_query($conn,$querycd);
+		$rowcd = mysqli_fetch_row($resultcd);
+		$permissionname = $rowcd[0];
+
+	if ($permissionname != NULL) {
+		if ($permissionname == "training"){
+		echo '	
 	 <h2 class="mb-4">Training Videos</h2>
      
 	<div class="container">
@@ -35,8 +49,11 @@
 	</div>
         
 
-	  ';
+		';
+	  }
+		
+		}
+	}
 	  ?>
 		
         
- 
