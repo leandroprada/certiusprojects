a8d0c0bc911bf2556e4988fed72427adc226cad8
leandroprada <leandroprada@gmail.com>
2020-05-24 15:13:33 -0300

    set permissions for username level

--- content/content_training_tips.php
+++ content/content_training_tips.php
@@ -1,9 +1,24 @@
 <?php
 
  session_start();
+require 'dbconn.inc.php';
  $userlogged = $_SESSION['name'];
- 
-	echo '
+$permissions = $_SESSION['permissions'];
+	//The code below parses course list and loops through it	
+	$permissions = explode(",",$permissions);
+
+foreach ($permissions as $p_id) 
+	{
+		$querycd1 = "SELECT permission_description FROM permissions WHERE p_id=";
+		$querycd2 = '"'.$p_id.'";';	
+		$querycd = $querycd1.$querycd2;
+		$resultcd = mysqli_query($conn,$querycd);
+		$rowcd = mysqli_fetch_row($resultcd);
+		$permissionname = $rowcd[0];
+
+	if ($permissionname != NULL) {
+		if ($permissionname == "training"){
+		echo '	
 	 <h2 class="mb-4">Training Material</h2>
      
 	<div class="container">
@@ -101,8 +116,12 @@
 	</div>
         
 
-	  ';
+	 
+		';
+	  }
+		
+		}
+	}
 	  ?>
 		
         
- 
