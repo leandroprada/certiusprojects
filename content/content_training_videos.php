a039a82e46c6b0faa72f7832e22fa31bef9513e5
leandroprada <leandroprada@gmail.com>
2020-06-01 13:03:35 -0300

    added Black DUck Hub training

--- content/content_training_videos.php
+++ content/content_training_videos.php
@@ -41,6 +41,21 @@ foreach ($permissions as $p_id)
                 <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Avaya CPaaS API webinar</h2>
 				<iframe width="75%" height="450px" src="../files/cpaas/AVAYA_Webinar_APIs_CPaaS_FernandezLopez_210520.mp4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controlsList="nodownload"></iframe>
 			  </div>
+			  
+			  <div class="video-promo-content text-center">
+                <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Black Duck Hub Fundamentals</h2>
+				<iframe width="75%" height="450px" src="../files/training/blackDuckHub06012020.mp4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<h3 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Black Duck Hub - otro material de soporte </h3>
+				<ul >
+					<li style="text-decoration:none">
+						<a  href="../content/soportevideos/TomcatConfiguration.pptx" target="_blank">Presentaci&oacute;n Black Duck Hub </a><a href="../content/soportevideos/TrainingBlackDuckHub.pdf" class="btn btn-info" style="color:white;">PDF</a><a href="../content/soportevideos/TrainingBlackDuckHub.pptx" class="btn btn-info" style="color:white;">PPT</a>
+						TrainingBlackDuckHub.pdf
+					</li>
+					<li style="text-decoration:none">
+						<a  href="../content/soportevideos/TomcatConfig.zip" target="_blank">Otra informaci&oacute;n &Uacute;til - Tomcat Configuration </a><a href="../content/soportevideos/TomcatConfig.zip" class="btn btn-info" style="color:white;">Ver</a>
+					</li>
+				</ul>
+              </div>
           </div>
         </div>
 	</div>
