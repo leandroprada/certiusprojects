460088f3a3b30676b8a542d265f5652f94466fa4
leandroprada <leandroprada@gmail.com>
2020-06-05 23:17:33 -0300

    added confidential toll free

--- content/content_confidential_tollfree.php
+++ content/content_confidential_tollfree.php
@@ -26,9 +26,8 @@ foreach ($permissions as $p_id)
           <div class="col-md-12 col-sm-12">
               <div class="text-left">
                 <h4 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Toll Free Forwarding</h4>
-				<a  class="btn btn-danger btn-sm " href="../../files/tollFree/tollfreenewaudios.rar" target="_blank" style="color:white;background-color:#C26861;width:200px;">Get Recordings for the IVR </a>
 				<ul style="list-style:none">
-				
+				<a  class="btn btn-danger btn-sm " href="../../files/tollFree/tollfreenewaudios.rar" target="_blank" style="color:white;background-color:#C26861;width:200px;padding-bottom25px;">Get Recordings for the IVR </a>
 					 
 					<li style="text-decoration:none;padding-bottom:10px; "><a  href="#tollFreeSubmenu1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;width:200px;">See Info </a>
 					
