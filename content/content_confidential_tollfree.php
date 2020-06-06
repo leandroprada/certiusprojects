c575a72e9a6ed35acb77a62dd042e88b6f460769
leandroprada <leandroprada@gmail.com>
2020-06-05 23:14:59 -0300

    added confidential toll free

--- content/content_confidential_tollfree.php
+++ content/content_confidential_tollfree.php
@@ -27,9 +27,9 @@ foreach ($permissions as $p_id)
               <div class="text-left">
                 <h4 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Toll Free Forwarding</h4>
 				<ul style="list-style:none">
-				<a  class="btn btn-danger btn-sm " href="../../files/tollFree/tollfreenewaudios.rar" target="_blank" style="color:white;background-color:#C26861">Abrir </a>
-					 Recordings for the IVR
-					<li style="text-decoration:none;padding-bottom:10px; "><a  href="#tollFreeSubmenu1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;">Ver Info </a>
+				<a  class="btn btn-danger btn-sm " href="../../files/tollFree/tollfreenewaudios.rar" target="_blank" style="color:white;background-color:#C26861;width:100px;">Get Recordings for the IVR </a>
+					 
+					<li style="text-decoration:none;padding-bottom:10px; "><a  href="#tollFreeSubmenu1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;width:100px;">See Info </a>
 					
 						<div class="collapse list-unstyled" id="tollFreeSubmenu1">
 						<h3>
