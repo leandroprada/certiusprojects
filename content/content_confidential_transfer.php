45ade0a4fcb272ab548965f650f0df5b93bc5eae
leandroprada <leandroprada@gmail.com>
2020-08-21 11:15:11 -0300

    removed transfer files and buttons

--- content/content_confidential_transfer.php
+++ content/content_confidential_transfer.php
@@ -21,9 +21,10 @@ foreach ($permissions as $p_id)
 		echo '	
 	 <h2 class="mb-4">Transfer Page</h2>
 	 
-	 <!-- <a  href="#" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;"> Download AAA RTM bundle</a> -->
+	 <h3>Below you will find your files, if they do not show up, contact leandro.prada@certiustech.com</h3>
+	 <!-- <a  href="#" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;"> Download AAA RTM bundle</a> 
 	 <a  href="../files/transfer/meetme-Bundle-08-21-20.zip" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;"> Download Prudential RTM bundle Meet Me</a>
-	 <a  href="../files/transfer/sharedServices-Bundle-08-21-20.zip" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;"> Download Prudential RTM bundle Shared Services</a>
+	 <a  href="../files/transfer/sharedServices-Bundle-08-21-20.zip" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;"> Download Prudential RTM bundle Shared Services</a>-->
 	  
 	 
 		';
@@ -33,7 +34,7 @@ foreach ($permissions as $p_id)
 		echo '	
 	 <h2 class="mb-4">Transfer Page</h2>
 	 
-	 <a  href="../files/transfer/cvs.zip" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;"> Download CVS Health POM bundle</a>
+	 <!--<a  href="../files/transfer/cvs.zip" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;"> Download CVS Health POM bundle</a>-->
 	 
 	 
 	 
