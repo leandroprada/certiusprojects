0add371ac8b3056dc61b7ffbd66c36409e4488bd
leandroprada <leandroprada@gmail.com>
2020-10-13 08:36:52 -0300

    changed training and KTs websites

--- content/content_training_videos.php
+++ content/content_training_videos.php
@@ -26,7 +26,7 @@ foreach ($permissions as $p_id)
           <div class="col-md-12 col-sm-12">
 			<div class="text-left">
 				<ul style="list-style:none">
-					<li style="text-decoration:none;padding-bottom:10px; "><a  href="https://certiustech.sharepoint.com/sites/KnowledgeDatabase/Documentos%20compartidos/Forms/AllItems.aspx?viewid=0ecd27b9%2D99be%2D4dbe%2Da1a3%2D874d70e7b535&id=%2Fsites%2FKnowledgeDatabase%2FDocumentos%20compartidos%2FKT%2DSessions%2FInternal%20Training%20KTs" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;">Ver </a>
+					<li style="text-decoration:none;padding-bottom:10px; "><a  href="https://certiustech.sharepoint.com/sites/KnowledgeDatabase/Documentos%20compartidos/Forms/AllItems.aspx" aria-expanded="false" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;">Ver </a>
 						Please visit the new sharepoint
 					</li>
 				</ul>
